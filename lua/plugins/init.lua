84b7193b0586e289cbe322f94c20f70d27d76cc9
Dayton-IX <max.dayton@protonmail.com>
2025-01-19 17:55:46 -0700

    init

---
+++ lua/plugins/init.lua
@@ -0,0 +1,39 @@
+
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- Uncomment and add treesitter if needed
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+
+  -- Add nvim-web-devicons for icon support
+  {
+    "kyazdani42/nvim-web-devicons",
+    config = function()
+      require'nvim-web-devicons'.setup {
+        -- Your options here (optional)
+        default = true;
+      }
+    end,
+  },
+}
+
